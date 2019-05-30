e9222619daeb491526f9ce18105974e2ea0c592d
Matt Schurenko <mschurenko@zenefits.com>
2019-05-29 17:17:31 -0700

    wtf?

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ VERSION='0.0.1'
 
 export GO111MODULE=on
 
-# GOOS=linux go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
-go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
+GOOS=linux go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
+#go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
