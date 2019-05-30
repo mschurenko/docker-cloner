21115621417fa8a8a5b55a3a909e254916ac85bd
Matt Schurenko <mschurenko@zenefits.com>
2019-05-30 13:11:10 -0700

    fix cross compile issue

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 
 VERSION='0.0.1'
 
-export GO111MODULE=on
-
-GOOS=linux go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
+GO111MODULE=on GOOS=linux CGO_ENABLED=0 go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
 #go build -ldflags "-X main.version=$VERSION" -o docker-cloner main.go
